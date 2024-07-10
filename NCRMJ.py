9702ad2efb371579e029fd097dcf3904c3256f49
Clearwave <fedoraerb@gmail.com>
2024-07-10 13:32:10 +0800

    upd: Support Standings Fetching

--- NCRMJ.py
+++ NCRMJ.py
@@ -1,6 +1,18 @@
 import requests,json
 import sys
 
+argv = sys.argv
+if '-h' in argv or '--help' in argv or len(argv) == 1:
+    print("Usage: NCRMJ.py [options] [file.cpp/file.py]")
+    print("Options:")
+    print("  -h, --help\t\tShow this help message and exit")
+    print("  -l [language]\t\tSpecify the language of the code")
+    print("  -p [problem]\t\tSpecify the problem name")
+    print("  -s [contest_id] [username]\t\tShow the status of the user in the contest")
+    print("Example:")
+    print("  NCRMJ.py -l C++(clang++ 11.0.1) -p A A.cpp")
+    exit()
+
 lang_map = {
     "C++(clang++ 11.0.1)":2,
     "Python3":11
@@ -45,13 +57,23 @@ def submit_code(problemName, code, languageID, languageName):
 
 #print(submit_code("B",code_A,2,"C++(clang++ 11.0.1)"))
 
-argv = sys.argv
+if '-s' in argv:
+    contest_id = argv[argv.index('-s')+1]
+    user_name = argv[argv.index('-s')+2]
+    r = requests.get(f"https://ac.nowcoder.com/acm-heavy/acm/contest/real-time-rank-data?token=&id={contest_id}&searchUserName={user_name}&limit=0",cookies=cookies)
+    x = json.loads(r.text)
+    x = x['data']['rankData'][0]
+    print(f"\033[34mRank: {x['ranking']} Accepted: {x['acceptedCount']} Score:{x['totalScore']} Penalty: {x['penaltyTime']//1000}")
+    print("Details:")
 
-if '-h' in argv or '--help' in argv or not '.cpp' in ' '.join(argv) and not '.py' in ' '.join(argv):
-    print("NCRMJ.py - NowCoder RemoteJudge by Clearwave")
-    print("Usage: NCRMJ.py [code] [-l language] [-p problem]")
-    print("Example: NCRMJ.py A.cpp -l C++ -p A")
-    sys.exit()
+    scoreList = x['scoreList']
+
+    for i in range(len(scoreList)):
+        print(f"{"\033[92m" if scoreList[i]['accepted'] == True else "\033[91m"}",end="")
+        print(f"Problem: {chr(65+i)} {"Accepted" if scoreList[i]['accepted'] == True else f'Unaccepted(-{scoreList[i]['failedCount']})'} Score: {scoreList[i]['score']}")
+    
+    print("\033[0m")
+    exit()
 
 for arg in argv:
     if '.cpp' in arg or '.py' in arg:
@@ -81,4 +103,10 @@ if '-p' in argv:
 print(f"Problem: {problemName}")
 print(f"Language: {languageName}")
 
-print(submit_code(problemName,code,languageID,languageName))
+res = submit_code(problemName,code,languageID,languageName)
+resHundredRate = int(res["rightHundredRate"])
+resDesc = res["desc"]
+#print(resHundredRate,resDesc)
+print("\033[34mScore: {}%: [".format(resHundredRate), "▋" * (resHundredRate // 2),' '* ((100-resHundredRate)//2),']')
+print(f"{"\033[92m" if resHundredRate == 100 else "\033[91m"}Description: {resDesc}")
+print("\033[0m")
